7694b5cdbaf60f3bda392b9065012dbbbe1b0f13
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-07-04 13:34:06 +0200

    Update buy.js

--- api/buy.js
+++ api/buy.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 
-router.get('/buy/lib', function(req, res, next) {
+router.get('/buy/lib', (req, res, next) => {
     res.sendFile(path.join(process.cwd(), 'node_modules', 'braintree-web', 'dist', 'braintree.js'));
 });
 
